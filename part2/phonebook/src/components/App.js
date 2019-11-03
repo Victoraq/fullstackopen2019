249c25cae05b67ff16334047016300435be7ca52
Victor Aquiles Soares de Barros Alencar <victoraquiles@ice.ufjf.br>
2019-11-03 09:44:23 -0200

    Exercise 2.8

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1234567'}
     ])
     const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
 
-    const numbers = () => persons.map(p => <p key={p.name}>{p.name}</p>)
+    const numbers = () => persons.map(p => <p key={p.name}>{p.name}  {p.number}</p>)
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
     }
 
     const addPerson = (event) => {
@@ -19,29 +24,30 @@ const App = () => {
         if (persons.find(p => p.name === newName)) {
             window.alert(`${newName} is already added to phonebook`)
             setNewName('')
+            setNewNumber('')
             return
         }
         
-        const newPerson = { name: newName }
+        const newPerson = { name: newName, number: newNumber }
 
         setPersons(persons.concat(newPerson))
         setNewName('')
+        setNewNumber('')
     }
 
     return (
-    <div>
-        <h2>Phonebook</h2>
-        <form onSubmit={addPerson}>
         <div>
-            name: <input value={newName} onChange={handleNameChange}/>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+            <div>name: <input value={newName} onChange={handleNameChange}/></div>
+            <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+            </form>
+            <h2>Numbers</h2>
+            {numbers()}
         </div>
-        <div>
-            <button type="submit">add</button>
-        </div>
-        </form>
-        <h2>Numbers</h2>
-        {numbers()}
-    </div>
     )
 }
 
