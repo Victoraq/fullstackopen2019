976ce7c9af5b0f72a038e732712c5d4a72c83958
Victor Aquiles <victoraqalencar@gmail.com>
2019-11-06 14:42:19 -0200

    Exercise 2.12

---
+++ part2/countriesdata/src/components/App.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const Search = ({search, handleSearch}) => {
+    return (
+        <div>
+            find countries <input value={search} onChange={handleSearch}></input>
+        </div>
+    )
+}
+
+
+const Country = ({country}) => {
+    return (
+        <div>
+            <p>{country.name}</p>
+        </div>
+    )
+}
+
+
+const Stats = ({country}) => {
+    
+    const languages = () => country.languages.map(lang => <li key={lang.name}>{lang.name}</li>)
+
+    return (
+        <div>
+            <h2>{country.name}</h2>
+
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+
+            <h3>languages</h3>
+            <ul>
+                {languages()}
+            </ul>
+
+            <img src={country.flag} alt="flag" width="10%" height="10%"/>
+
+        </div>
+    )
+}
+
+
+const Countries = ({countries, filterBy}) => {
+
+    const toShow = () => {
+        const countriesToShow = countries.filter(
+                country => country.name.toLowerCase().includes(filterBy)
+            )
+        
+        if (countriesToShow.length === 1) {
+            return <Stats country={countriesToShow[0]}/>
+        } else {
+            return countriesToShow.map(country => <Country key={country.name} country={country}/>)
+        }
+    }
+
+    const countriesFound = toShow()
+
+    if (filterBy === '') {
+        return ''
+    }
+    if (countriesFound.length > 10) {
+        return <p>Too many matches, specify another filter</p>
+    } else {
+        return countriesFound
+    }
+}
+
+
+const App = () => {
+    const [ countries, setCountries] = useState([])
+    const [ findCountry, setFindCountry ] = useState('')
+
+
+    // get data from the server
+    const getData = () => {
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => setCountries(response.data))
+    }
+    
+    useEffect(getData,[])
+
+
+    const handleFinder = (event) => {
+        setFindCountry(event.target.value.toLowerCase())
+    }
+
+
+    return (
+        <div>
+            <Search search={findCountry} handleSearch={handleFinder}/>
+            <Countries countries={countries} filterBy={findCountry}/>
+        </div>
+    )
+}
+
+export default App
