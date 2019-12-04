16ea8aa4de06ea49c0e96e569cfbb9ae56f91878
Victor Aquiles <victoraqalencar@gmail.com>
2019-12-04 13:56:36 -0200

    Exercise 3.4

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -55,6 +55,14 @@ app.get('/info', (request, response) => {
 })
 
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phoneList["persons"] = phoneList["persons"].filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
