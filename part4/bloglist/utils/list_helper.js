8d0cd15f786d38bcdcaad2af77848794d3ee5bf8
Victor Aquiles <victoraqalencar@gmail.com>
2020-03-25 17:53:11 -0300

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const findGreatest = (acc, item) => {
+        return acc.likes > item.likes ? acc : item
+    }
+
+    favorite = blogs.reduce(findGreatest,1)
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
