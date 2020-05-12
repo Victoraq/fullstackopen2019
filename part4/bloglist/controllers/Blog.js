7d753966ebfd3953950e90d71716b32e1f1f14aa
Victor Aquiles <victoraqalencar@gmail.com>
2020-05-12 11:55:08 -0300

    Exercise 4.13

--- part4/bloglist/controllers/Blog.js
+++ part4/bloglist/controllers/Blog.js
@@ -1,27 +1,26 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/Blog')
 
-blogRouter.get('/', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
+blogRouter.get('/', async (request, response) => {
+	const blogs = await Blog.find({})
+	response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
 
 	if ('title' in request.body & 'url' in request.body) {
 		const blog = new Blog(request.body)
 
-		blog
-			.save()
-			.then(result => {
-				response.status(201).json(result)
-			})
+		const savedBlog = await blog.save()
+		response.status(201).json(savedBlog.toJSON())
 	} else {
 		response.status(400).json('ValidatonError')
 	}
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+	await Blog.findByIdAndRemove(request.params.id)
+	response.status(204).end()
 })
 
 module.exports = blogRouter
--- part4/bloglist/tests/note_api.test.js
+++ part4/bloglist/tests/note_api.test.js
@@ -16,74 +16,102 @@ beforeEach(async () => {
     await Promise.all(promiseArray)
 })
 
-test('blog are returned as json', async () => {
-    await api
-        .get('/api/bloglist')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-
-test('blog list returns the correct amount of blogs', async () => {
-    const response = await api.get('/api/bloglist')
-
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('id property is named correctly', async () => {
-    const response = await api.get('/api/bloglist')
-
-    const blog = response.body[0]
-
-    expect(blog.id).toBeDefined()
-})
-
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        "title": "some new blog",
-        "author": "the new master",
-        "url": "newmasterblog.com",
-        "likes": "8999"
-    }
-
-    await api.post('/api/bloglist')
-            .send(newBlog)
-            .expect(201)
+describe('when there is initially some blogs saved', () => {
+    test('blog are returned as json', async () => {
+        await api
+            .get('/api/bloglist')
+            .expect(200)
             .expect('Content-Type', /application\/json/)
+    })
+    
+    test('blog list returns the correct amount of blogs', async () => {
+        const response = await api.get('/api/bloglist')
+    
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+    
+})
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-    const titles = blogsAtEnd.map(blog => blog.title)
-    expect(titles).toContain('some new blog')
+describe('viewing a specific blog', () => {
+    test('id property is named correctly', async () => {
+        const response = await api.get('/api/bloglist')
+    
+        const blog = response.body[0]
+    
+        expect(blog.id).toBeDefined()
+    })
 
 })
 
-test('default value of likes is 0', async () => {
-    const newBlog = {
-        "title": "some new blog",
-        "author": "the new master",
-        "url": "newmasterblog.com"
-    }
+describe('addition of a new note', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            "title": "some new blog",
+            "author": "the new master",
+            "url": "newmasterblog.com",
+            "likes": "8999"
+        }
+    
+        await api.post('/api/bloglist')
+                .send(newBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+    
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    
+        const titles = blogsAtEnd.map(blog => blog.title)
+        expect(titles).toContain('some new blog')
+    
+    })
 
-    await api.post('/api/bloglist')
-            .send(newBlog)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
-    const blogsAtEnd = await helper.blogsInDb()
-    const addedBlog = blogsAtEnd.find(blog => blog.title == newBlog.title)
-
-    expect(addedBlog.likes).toEqual(0)
+    test('default value of likes is 0', async () => {
+        const newBlog = {
+            "title": "some new blog",
+            "author": "the new master",
+            "url": "newmasterblog.com"
+        }
+    
+        await api.post('/api/bloglist')
+                .send(newBlog)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+        const blogsAtEnd = await helper.blogsInDb()
+        const addedBlog = blogsAtEnd.find(blog => blog.title == newBlog.title)
+    
+        expect(addedBlog.likes).toEqual(0)
+    })
+    
+    test('title and url missing from blog returns bad request', async () => {
+        const missingPropBlog = {
+            "author": "the new master"
+        }
+    
+        await api.post('/api/bloglist')
+                .send(missingPropBlog)
+                .expect(400)
+    
+    })
 })
 
-test('title and url missing from blog returns bad request', async () => {
-    const missingPropBlog = {
-        "author": "the new master"
-    }
+describe('deletion of a note', () => {
+    test('succeeds with status 204 if id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
 
-    await api.post('/api/bloglist')
-            .send(missingPropBlog)
-            .expect(400)
+        await api.delete(`/api/bloglist/${blogToDelete.id}`)
+                .expect(204)
 
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(
+            helper.initialBlogs.length - 1
+        )
+
+        const titles = blogsAtEnd.map(blog => blog.title)
+
+        expect(titles).not.toContain(blogToDelete.title)
+    })
 })
 
 afterAll(() => {
